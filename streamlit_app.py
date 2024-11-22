ec634d95b06c7a9da343c99be18ea9a2ef75a771
Rohan Patil <rohanvp009@gmail.com>
2024-11-22 19:46:50 +0000

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Set the title of the application
+#st.title("Fullscreen iFrame Stream")
+
+# Embed the iframe HTML code directly into Streamlit using markdown
+iframe_code = """
+    <iframe src="https://embedsports.me/test-odi/west-indies-vs-bangladesh-stream-1" 
+            allowfullscreen="true" allowtransparency="true" 
+            width="100%" height="100%" frameborder="0">
+    </iframe>
+"""
+
+# Display the iframe in the Streamlit app using markdown with raw HTML
+st.markdown(iframe_code, unsafe_allow_html=True)
