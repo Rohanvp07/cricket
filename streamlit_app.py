ffcbcc6d52b1652c31be0a401862a7c46c7c1146
Rohan Patil <rohanvp009@gmail.com>
2024-12-26 23:14:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 # Embed the iframe HTML code directly into Streamlit using markdown
 iframe_code = """
-    <iframe src='https://embedsports.me/epl/newcastle-united-vs-aston-villa-stream-1'
+    <iframe src='https://embedsports.me/test-odi/boxing-day-australia-vs-india-stream-1'
             allowfullscreen="true" allowtransparency="true" 
             width="100%" height="100%" frameborder="0">
     </iframe>
